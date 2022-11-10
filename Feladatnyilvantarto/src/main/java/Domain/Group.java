835e57548f641e030c496fa5c86b6d8e0f9c3a81
WakooMan <magyar.viktor.2000@gmail.com>
2022-11-10 13:04:30 +0100

    added User Class

--- Feladatnyilvantarto/src/main/java/Domain/Group.java
+++ Feladatnyilvantarto/src/main/java/Domain/Group.java
@@ -1,4 +1,15 @@
 package Domain;
 
+import java.util.ArrayList;
+
 public class Group {
+    private ArrayList<Ticket> tickets;
+    public Group()
+    {
+        tickets = new ArrayList<Ticket>();
+    }
+    public ArrayList<Ticket> getTickets()
+    {
+        return tickets;
+    }
 }
--- Feladatnyilvantarto/src/main/java/Domain/User.java
+++ Feladatnyilvantarto/src/main/java/Domain/User.java
@@ -1,4 +1,53 @@
 package Domain;
 
-public class User {
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+public class User
+{
+    private int id;
+    private Credentials credentials;
+    private ArrayList<Group> groups;
+    private UserType userType;
+
+    public User(int id,Credentials credentials,UserType userType)
+    {
+        this.id = id;
+        this.credentials = credentials;
+        this.userType = userType;
+        this.groups = new ArrayList<Group>();
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public UserType getUserType()
+    {
+        return userType;
+    }
+
+    public Credentials getCredentials()
+    {
+        return credentials;
+    }
+    public void setCredentials(Credentials cred)
+    {
+        this.credentials = cred;
+    }
+    public ArrayList<Group> getGroups()
+    {
+        return groups;
+    }
+
+    public ArrayList<Ticket> getAssignedTickets()
+    {
+        ArrayList<Ticket> result = new ArrayList<Ticket>();
+        for(Group group:groups)
+        {
+            result.addAll(group.getTickets().stream().filter((t) -> (userType == UserType.Worker)?t.getAssignees().contains(this):t.getAssigner().equals(this)).collect(Collectors.toList()));
+        }
+        return result;
+    }
 }
