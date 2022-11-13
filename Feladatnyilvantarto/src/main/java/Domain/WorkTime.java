394bcb3130f6db1c073315b9d57a80825aa69ae3
daffodea <68898321+daffodea@users.noreply.github.com>
2022-11-13 17:24:11 +0100

    add WorkTime class

--- Feladatnyilvantarto/src/main/java/Domain/WorkTime.java
+++ Feladatnyilvantarto/src/main/java/Domain/WorkTime.java
@@ -1,4 +1,38 @@
 package Domain;
 
+
+import java.time.ZonedDateTime;
+import java.time.temporal.ChronoUnit;
+
 public class WorkTime {
+    private final ZonedDateTime startDate;
+    private ZonedDateTime endDate;
+    private TimeMeasure timeMeasure;
+    private int timeWorked;
+
+    public WorkTime(TimeMeasure timeMeasure){
+        this.timeMeasure=timeMeasure;
+        startDate= ZonedDateTime.now();
+    }
+    public ZonedDateTime getStartDate() {
+        return startDate;
+    }
+
+    public void end(){
+        endDate=ZonedDateTime.now();
+        timeWorked=(int)startDate.until(endDate, ChronoUnit.MINUTES);
+
+    }
+
+    public int getTimeWorked() {
+        return timeWorked;
+    }
+
+    public ZonedDateTime getEndDate() {
+        return endDate;
+    }
+
+    public TimeMeasure getTimeMeasure() {
+        return timeMeasure;
+    }
 }
