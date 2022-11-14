617f9d8ee55b5dc8c284f0fd78491cf346594ed4
WakooMan <magyar.viktor.2000@gmail.com>
2022-11-14 10:19:34 +0100

    implemented group and credentials class

--- Feladatnyilvantarto/src/main/java/Domain/Credentials.java
+++ Feladatnyilvantarto/src/main/java/Domain/Credentials.java
@@ -1,4 +1,25 @@
 package Domain;
 
 public class Credentials {
+    private String loginName;
+    private String password;
+
+    public void setPassword(String pw)
+    {
+        password = pw;
+    }
+    public String getPassword()
+    {
+        return password;
+    }
+
+    public String getLoginName()
+    {
+        return loginName;
+    }
+
+    public void setLoginName(String loginName)
+    {
+        this.loginName = loginName;
+    }
 }
--- Feladatnyilvantarto/src/main/java/Domain/Group.java
+++ Feladatnyilvantarto/src/main/java/Domain/Group.java
@@ -1,15 +1,66 @@
 package Domain;
 
+import java.security.InvalidParameterException;
 import java.util.ArrayList;
 
 public class Group {
     private ArrayList<Ticket> tickets;
-    public Group()
+    private User leader;
+    private ArrayList<User> workers;
+    public Group(User leader)
     {
+        if(leader.getUserType() != UserType.Leader)
+        {
+            throw new InvalidParameterException("Worker cannot be leader in a group!");
+        }
+        this.leader = leader;
+        workers = new ArrayList<User>();
         tickets = new ArrayList<Ticket>();
+    }
+
+    public User getLeader()
+    {
+        return leader;
+    }
+
+    public ArrayList<User> getWorkers()
+    {
+        return workers;
     }
     public ArrayList<Ticket> getTickets()
     {
         return tickets;
+    }
+
+    public void addTicket(Ticket ticket)
+    {
+        if(!tickets.contains(ticket))
+        {
+            tickets.add(ticket);
+        }
+    }
+
+    public void removeTicket(Ticket ticket)
+    {
+        if(tickets.contains(ticket))
+        {
+            tickets.remove(ticket);
+        }
+    }
+
+    public void addWorker(User user)
+    {
+        if(user.getUserType() == UserType.Worker && !workers.contains(user))
+        {
+            workers.add(user);
+        }
+    }
+
+    public void removeWorker(User user)
+    {
+        if(user.getUserType() == UserType.Worker && workers.contains(user))
+        {
+            workers.remove(user);
+        }
     }
 }
